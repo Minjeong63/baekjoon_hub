f2e5ebe805237aa9d6f2b0e455634c01dcff0f32
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2023-07-18 19:58:44 +0900

    [Bronze V] Title: 고양이, Time: 76 ms, Memory: 11212 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws IOException
+	{
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
