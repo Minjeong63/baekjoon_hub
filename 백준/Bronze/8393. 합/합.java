f7ff3b576bcc9129777a62010a593c81db5fec34
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2023-07-19 09:54:22 +0900

    [Bronze V] Title: 합, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int sum = 0;
+		for (int i=1; i<=n; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
