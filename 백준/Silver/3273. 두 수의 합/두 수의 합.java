9f5b57805e0dbb5a775e524ef3fd87c2fef34197
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2023-07-27 21:56:23 +0900

    [Silver III] Title: 두 수의 합, Time: 216 ms, Memory: 26920 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,38 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		ArrayList<Integer> inputArr = new ArrayList();
+		boolean[] inputArr = new boolean[2000000];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int x = Integer.parseInt(br.readLine());
 
 		for (int i=0; i<n; i++) {
-			inputArr.add(Integer.parseInt(st.nextToken()));
+			int key = Integer.parseInt(st.nextToken());
+			inputArr[key] = true;
 		}
 		
-		Collections.sort(inputArr);
-
-		int i = 0, j = n-1;
-		int pointNumber = 0;
-
-		while (i < j) {
-			if (inputArr.get(i) + inputArr.get(j) == x) {
-				pointNumber++;
-				i++;
-				j--;
-			} else if (inputArr.get(i) + inputArr.get(j) < x) {
-				i++;
-			} else {
-				j--;
+		int cnt = 0;
+		
+		for (int i=1; i<x; i++) {
+			if (inputArr[i] && inputArr[x-i]) {
+				cnt++;
 			}
 		}
-		System.out.println(pointNumber);
+		System.out.println(cnt/2);
 	}
 }
