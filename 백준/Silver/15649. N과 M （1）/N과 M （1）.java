34b54d95ebd499bbe0fd3cefcfdf1375dd85e6af
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2023-08-26 14:35:50 +0900

    [Silver III] Title: N과 M (1), Time: 2140 ms, Memory: 62632 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int[] permArr;
+	static boolean[] isSelectedArr;
+	static int N, M;
+	
+	static void perm(int idx) {
+		
+		if (idx == M) {
+			for (int i=0; i<permArr.length; i++) {
+				System.out.print(permArr[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for (int i=1; i<=N; i++) {
+			if (!isSelectedArr[i]) {
+				permArr[idx] = i;
+				isSelectedArr[i] = true;
+				perm(idx+1);
+				isSelectedArr[i] = false;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		permArr = new int[M];
+		isSelectedArr = new boolean[N+1];
+		
+		perm(0);
+		
+	}
+
+}
