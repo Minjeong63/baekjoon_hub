c221fb545f3dd0b0c336aeb8c916ff38cd04ee5f
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2023-10-04 15:40:34 +0900

    [Bronze I] Title: 평균, Time: 88 ms, Memory: 11968 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,28 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-     	int N = Integer.parseInt(br.readLine());   
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	ArrayList<Double> input = new ArrayList<Double>();
-    	double max = Double.MIN_VALUE;
-    	for (int i=0; i<N; i++) {
-    		double input2 = Double.valueOf(st.nextToken());
-    		input.add(input2);
-    		if (input2 > max) max = input2;
-    	}
-    	double[] average = new double[N];
-    	for (int i=0; i<N; i++) {
-    		average[i] = Double.valueOf(input.get(i) / max * 100);
-    	}
-    	double sum = 0.0;
-    	for (int i=0; i<N; i++) {
-    		sum += average[i];
-    	}
-    	System.out.println(sum / N);
-    }
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int N = Integer.parseInt(br.readLine());
+		String[] inputList = br.readLine().split(" ");
+
+		float max = 0;
+		for (int i=0; i<inputList.length; i++) {
+			max = Math.max(max, Integer.parseInt(inputList[i]));
+		}
+
+		float sum = 0;
+		for (int i=0; i<inputList.length; i++) {
+			sum += (Integer.parseInt(inputList[i]) / max * 100);
+		}
+
+		System.out.println(sum / N);
+
+	}
 }
