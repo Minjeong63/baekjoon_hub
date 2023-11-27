62cb9f06d1f6910a0acdbf6abce5c41b4ccbe5c7
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2023-11-27 10:28:50 +0900

    [Silver IV] Title: 카드 놓기, Time: 172 ms, Memory: 13168 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.java
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.java
@@ -5,71 +5,31 @@ import java.util.ArrayList;
 
 public class Main {
     static ArrayList<Integer> result;
+    static int n, k;
+    static boolean[] visited;
+    static int[] card;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int k = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
 
-        int[] card = new int[n];
+        card = new int[n];
         for (int i = 0; i < n; i++) {
             card[i] = Integer.parseInt(br.readLine());
         }
-
+        visited = new boolean[n];
         result = new ArrayList<>();
-        if (k == 2) {
-            for (int i = 0; i < n - 1; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    int[] arr = {card[i], card[j]};
-                    int[] output = new int[k];
-                    boolean[] visited = new boolean[k];
 
-                    perm(arr, output, visited, 0, k);
-                }
-            }
-        }
-
-        if (k == 3) {
-            for (int i = 0; i < n - 2; i++) {
-                for (int j = i + 1; j < n - 1; j++) {
-                    for (int l = j + 1; l < n; l++) {
-                        int[] arr = {card[i], card[j], card[l]};
-                        int[] output = new int[k];
-                        boolean[] visited = new boolean[k];
-
-                        perm(arr, output, visited, 0, k);
-                    }
-                }
-            }
-        }
-
-        if (k == 4) {
-            for (int i = 0; i < n - 3; i++) {
-                for (int j = i + 1; j < n - 2; j++) {
-                    for (int l = j + 1; l < n - 1; l++) {
-                        for (int m = l + 1; m < n; m++) {
-                            int[] arr = {card[i], card[j], card[l], card[m]};
-                            int[] output = new int[k];
-                            boolean[] visited = new boolean[k];
-
-                            perm(arr, output, visited, 0, k);
-                        }
-                    }
-                }
-            }
-        }
+        perm("", 0);
         System.out.println(result.size());
     }
 
-    static void perm(int[] arr, int[] output, boolean[] visited, int depth, int n) {
-        if (depth == n) {
-            String orderNumber = "";
-            for (int i = 0; i < output.length; i++) {
-                orderNumber += output[i];
-            }
-            if (!result.contains(Integer.parseInt(orderNumber))) {
-                result.add(Integer.parseInt(orderNumber));
+    static void perm(String resultStr, int depth) {
+        if (depth == k) {
+            if (!result.contains(Integer.parseInt(resultStr))) {
+                result.add(Integer.parseInt(resultStr));
             }
             return;
         }
@@ -77,8 +37,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                output[depth] = arr[i];
-                perm(arr, output, visited, depth + 1, n);
+                perm(resultStr + card[i], depth + 1);
                 visited[i] = false;
             }
         }
