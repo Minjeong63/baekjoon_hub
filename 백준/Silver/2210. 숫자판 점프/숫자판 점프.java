057d34d8ea740f0407abbc4a6637764b123b5be6
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2024-01-15 18:40:46 +0900

    [Silver II] Title: 숫자판 점프, Time: 112 ms, Memory: 15316 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
@@ -1,33 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
-    static String[][] numberBoard = new String[5][5];
-    static ArrayList<String> result = new ArrayList<>();
+    static int[][] numberBoard = new int[5][5];
+    static boolean[] visited = new boolean[1000000];
+    static int result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < 5; i++) {
             String[] input = br.readLine().split(" ");
             for (int j = 0; j < 5; j++) {
-                numberBoard[i][j] = input[j];
+                numberBoard[i][j] = Integer.parseInt(input[j]);
             }
         }
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                dfs(i, j, numberBoard[i][j]);
+                dfs(i, j, String.valueOf(numberBoard[i][j]));
             }
         }
-        System.out.println(result.size());
+        System.out.println(result);
     }
 
-    static void dfs(int x, int y, String str) {
-        if (str.length() == 6) {
-            if (!result.contains(str)) {
-                result.add(str);
+    static void dfs(int x, int y, String num) {
+        if (num.length() == 6) {
+            if (!visited[Integer.parseInt(num)]) {
+                visited[Integer.parseInt(num)] = true;
+                result++;
             }
             return;
         }
@@ -38,7 +39,7 @@ public class Main {
             int xVal = x + dx[i];
             int yVal = y + dy[i];
             if (xVal >= 0 && yVal >= 0 && xVal < 5 && yVal < 5) {
-                dfs(xVal, yVal, str + numberBoard[xVal][yVal]);
+                dfs(xVal, yVal, num + numberBoard[xVal][yVal]);
             }
         }
     }
