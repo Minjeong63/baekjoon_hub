d02becf7240671bb37613c036b2af69284bf9aa3
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2024-01-16 20:57:36 +0900

    [Gold IV] Title: 이분 그래프, Time: 880 ms, Memory: 282636 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -5,74 +5,63 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer>[] graph;
     static int[] visited;
-    static boolean isGraph;
+    static ArrayList<Integer>[] graph;
+    static boolean isBipartiteGraph;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int K = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < K; i++) {
+            isBipartiteGraph = true;
             StringTokenizer st = new StringTokenizer(br.readLine());
             int V = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
-
             visited = new int[V + 1];
-            isGraph = true;
-
             graph = new ArrayList[V + 1];
-            for (int k = 1; k <= V; k++) {
-                graph[k] = new ArrayList<>();
+
+            for (int j = 1; j <= V; j++) {
+                graph[j] = new ArrayList<>();
             }
 
             for (int j = 0; j < E; j++) {
-                String[] input = br.readLine().split(" ");
-                int u = Integer.parseInt(input[0]);
-                int v = Integer.parseInt(input[1]);
+                st = new StringTokenizer(br.readLine());
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
                 graph[u].add(v);
                 graph[v].add(u);
             }
 
             for (int j = 1; j <= V; j++) {
-                if (!isGraph) {
-                    sb.append("NO").append("\n");
-                    break;
-                }
-                if (visited[j] == 0) {
+                if (isBipartiteGraph && visited[j] == 0) {
                     dfs(j, 1);
                 }
             }
-            if (isGraph) {
-                sb.append("YES").append("\n");
+
+            if (isBipartiteGraph) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
             }
         }
-        System.out.println(sb);
     }
 
     static void dfs(int n, int color) {
-        if (!isGraph) {
+        if (!isBipartiteGraph) {
             return;
         }
+
         visited[n] = color;
-
         for (int num : graph[n]) {
-            // 방문하지 않은 경우
             if (visited[num] == 0) {
-                if (color == 1) {
-                    dfs(num, 2);
-                } else {
-                    dfs(num, 1);
-                }
-            }
+                dfs(num, 3 - color);
 
-            // 방문했는데 인접한 경우
-            if (visited[num] == color) {
-                isGraph = false;
-                return;
+            } else {
+                if (visited[num] == color) {
+                    isBipartiteGraph = false;
+                    return;
+                }
             }
         }
     }
