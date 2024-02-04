2b9d5c58eafa3ec5d941a2ccc87b6a5a17663ab5
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2024-02-04 12:19:18 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 12088 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -4,14 +4,10 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[][] color;
-    static int min = Integer.MAX_VALUE;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        color = new int[N][3];
+        int N = Integer.parseInt(br.readLine());
+        int[][] color = new int[N][3];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
