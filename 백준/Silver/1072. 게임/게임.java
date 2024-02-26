44c586e8b1af9eda46ea9536617510e531911289
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2024-02-26 09:48:37 +0900

    [Silver III] Title: 게임, Time: 76 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -2,14 +2,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 게임을 최소 몇 번 더 해야 형택이의 승률(Z)이 변하는 지 구하는 문제
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
+        // 게임 횟수
         int X = Integer.parseInt(input[0]);
+        // 이긴 게임 횟수
         int Y = Integer.parseInt(input[1]);
 
-        long winRate = (long) Math.floor((double) Y * 100 / X);
+        // 승률
+        long Z = (long) Math.floor((double) Y * 100 / X);
 
         int left = 1;
         int right = 1_000_000_000;
@@ -18,7 +22,7 @@ public class Main {
             int mid = (left + right) / 2;
             long newRate = (long) Math.floor((double) (Y + mid) * 100 / (X + mid));
 
-            if (newRate > winRate) {
+            if (newRate > Z) {
                 result = mid;
                 right = mid - 1;
             } else {
