9815d8a94b91ad31b913b71b1e2eaea9b368d62e
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2024-02-26 15:51:19 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 2444 ms, Memory: 88512 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int m;
+    static int[][] map;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int x = 0;
+        int y = 0;
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 2) {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        bfs(x, y);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] != 2 && map[i][j] != 0 && answer[i][j] == 0) {
+                    answer[i][j] = -1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(answer[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void bfs(int x, int y) {
+        answer = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(x, y));
+        visited[x][y] = true;
+        int dist = 1;
+
+        while (!q.isEmpty()) {
+            int qSize = q.size();
+            for (int j = 0; j < qSize; j++) {
+                Node node = q.poll();
+                for (int i = 0; i < 4; i++) {
+                    int nx = node.x + dx[i];
+                    int ny = node.y + dy[i];
+
+                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] && map[nx][ny] != 0) {
+                        q.add(new Node(nx, ny));
+                        answer[nx][ny] = dist;
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+            dist++;
+        }
+    }
+
+    static class Node {
+        int x;
+        int y;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
