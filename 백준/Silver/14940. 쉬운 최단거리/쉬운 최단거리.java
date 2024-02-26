b18cdc8e69f8ca262310e71a61a72c3caeea4601
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2024-02-26 15:57:52 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 672 ms, Memory: 83944 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -20,8 +20,10 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
+        // 목표지점 x, y 좌표
         int x = 0;
         int y = 0;
+        // 지도 배열
         map = new int[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -36,9 +38,10 @@ public class Main {
 
         bfs(x, y);
 
+        // 원래 갈 수 있는 땅인 부분 중에서 도달할 수 없는 위치에 -1 대입
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (map[i][j] != 2 && map[i][j] != 0 && answer[i][j] == 0) {
+                if (map[i][j] == 1 && answer[i][j] == 0) {
                     answer[i][j] = -1;
                 }
             }
@@ -60,6 +63,7 @@ public class Main {
         Queue<Node> q = new LinkedList<>();
         q.add(new Node(x, y));
         visited[x][y] = true;
+        // 목표지점에서의 거리
         int dist = 1;
 
         while (!q.isEmpty()) {
@@ -70,7 +74,8 @@ public class Main {
                     int nx = node.x + dx[i];
                     int ny = node.y + dy[i];
 
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] && map[nx][ny] != 0) {
+                    // index가 범위 안에 있으며 방문한 적이 없고 지도 값이 1일 때
+                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] && map[nx][ny] == 1) {
                         q.add(new Node(nx, ny));
                         answer[nx][ny] = dist;
                         visited[nx][ny] = true;
