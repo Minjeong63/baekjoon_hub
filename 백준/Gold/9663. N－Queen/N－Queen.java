3ffb574ebc76852d36c1c5ce34534d8cd25daa24
Minjeong63 <97074298+Minjeong63@users.noreply.github.com>
2024-07-09 17:12:10 +0900

    [Gold IV] Title: N-Queen, Time: 5436 ms, Memory: 15572 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -4,40 +4,70 @@ import java.io.InputStreamReader;
 
 public class Main {
     static int N;
-    static int[] arr;
-    static int result;
+    static int[][] visited;
+    static boolean[] visitedColumn;
+    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        backtracking(0);
-        System.out.println(result);
+        visited = new int[N][N];
+        visitedColumn = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            visited[0][i] = 1;
+            visitedColumn[i] = true;
+            dfs(0, 1);
+            visited[0][i] = 0;
+            visitedColumn[i] = false;
+        }
+        System.out.println(answer);
     }
 
-    static void backtracking(int depth) {
+    static void dfs(int x, int depth) {
         if (depth == N) {
-            result++;
+            answer++;
             return;
         }
 
         for (int i = 0; i < N; i++) {
-            arr[depth] = i;
-
-            if (isPossible(depth)) {
-                backtracking(depth + 1);
+            if (visitedColumn[i]) {
+                continue;
             }
-        }
-    }
 
-    static boolean isPossible(int depth) {
-        for (int i = 0; i < depth; i++) {
-            if (arr[depth] == arr[i]) {
-                return false;
-            } else if (Math.abs(depth - i) == Math.abs(arr[depth] - arr[i])) {
-                return false;
+            boolean crossVisited = false;
+            for (int j = Math.min(x + 1, i); j > 0; j--) {
+                if (visited[x + 1 - j][i - j] == 1) {
+                    crossVisited = true;
+                    break;
+                }
             }
+
+            int j = x + 1;
+            int k = i;
+            while (true) {
+                j--;
+                k++;
+
+                if (j < 0 || k < 0 || j >= N || k >= N) {
+                    break;
+                }
+
+                if (visited[j][k] == 1) {
+                    crossVisited = true;
+                    break;
+                }
+            }
+
+            if (crossVisited) {
+                continue;
+            }
+
+            visited[x + 1][i] = 1;
+            visitedColumn[i] = true;
+            dfs(x + 1, depth + 1);
+            visited[x + 1][i] = 0;
+            visitedColumn[i] = false;
         }
-        return true;
     }
 }
